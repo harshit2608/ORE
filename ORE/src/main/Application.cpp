75e074eadd422b755831d4e4f30b9d1010127bee
Harshit <harshitrock111@gmail.com>
2021-11-19 05:50:43 +0530

    First Triangle!

--- ORE/src/main/Application.cpp
+++ ORE/src/main/Application.cpp
@@ -1,14 +1,154 @@
 #include "../ORE.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+uint32_t SCREEN_WIDTH = 800;
+uint32_t SCREEN_HEIGHT = 600;
+std::string TITLE = "ORE";
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const char *vertexShaderSource1 = R"(
+#vertex 450 core\n
+layout (location = 0) in vec3 aPos;\n
+void main()\n
+{\n
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n
+})";
+
+const char *fragmentShaderSource1 = R"(
+#vertex 450 core\n
+out vec4 FragColor;\n
+void main()\n
+{\n
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n
+})";
+
+const char *vertexShaderSource = "#version 450 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char *fragmentShaderSource = "#version 450 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
 int main()
 {
     ORE::Log::Init();
-    ORE_CORE_TRACE("Project Initialization!! - TRACE");
-    ORE_CORE_INFO("Project Initialization!! - INFO");
-    ORE_CORE_WARN("Project Initialization!! - WARN");
-    ORE_CORE_ERROR("Project Initialization!! - ERROR");
-    ORE_CORE_CRITICAL("Project Initialization!! - CRITICAL");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    std::cout << "Project Initialization!!";
-    std::cin.get();
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE.c_str(), nullptr, nullptr);
+    if (window == nullptr)
+    {
+        ORE_CORE_ERROR("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        ORE_CORE_ERROR("Failed to initialize GLAD");
+        return -1;
+    }
+
+    uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    int32_t success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        ORE_CORE_ERROR("VERTEX_SHADER::COMPILATION_FAILED\n", infoLog);
+    }
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        ORE_CORE_ERROR("FRAGMENT_SHADER::COMPILATION_FAILED\n", infoLog);
+    }
+
+    uint32_t shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, nullptr, infoLog);
+        ORE_CORE_ERROR("SHADER::LINK_FAILED\n", infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    uint32_t VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
